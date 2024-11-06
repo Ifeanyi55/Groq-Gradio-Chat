7f676b04c8e221768344cd9d1112b14a89204f5a
Ifeanyi <72707830+Ifeanyi55@users.noreply.github.com>
2024-11-06 16:18:01 +0100

    Application file

---
+++ app.py
@@ -0,0 +1,15 @@
+import gradio as gr
+import groq_gradio
+import os
+
+os.environ["GROQ_API_KEY"] = "YOUR GROQ API_KEY"
+gr.load(
+    name = "llama-3.2-3b-preview",
+    src = groq_gradio.registry,
+    title = "Groq-Gradio Chat",
+    theme =  "upsatwal/mlsc_tiet",
+    examples = ["Tell me a short story about a puppy",
+                "Write a 14 line poem about travelling in Shakespeare style",
+                "What are the wonders of the world?",
+                "List the countries in Africa and their capitals"]
+).launch()
